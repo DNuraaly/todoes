38ae612a8ce834820683f5088adb0c538867481b
nuraaly <Dzhuzupbekovnuraaly@gmail.com>
2022-08-10 16:26:04 +0600

    remake note entity

--- app/Http/Controllers/Api/V1/NoteController.php
+++ app/Http/Controllers/Api/V1/NoteController.php
@@ -3,69 +3,121 @@
 namespace App\Http\ Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use App\Services\NoteServices;
+use App\Http\Requests\StoreNoteRequest;
+use App\Http\Requests\UpdateNoteRequest;
+use App\Services\NotesService;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 
 class NoteController extends Controller
 {
+    private const MESSAGES = [
+        'notFound' => 'Note not found.',
+        'deleted'  => 'Successfully, note deleted.'
+    ];
     /**
      * Display a listing of the resource..
      * @param Request $request
-     * @param NoteServices $note
+     * @param NotesService $notesService
      * @return JsonResponse
      */
-    public function index(Request $request, NoteServices $note): JsonResponse
+    public function index(Request $request, NotesService $notesService): JsonResponse
     {
-        return response()->json($note->getAllNotes($request));
+        $user = $request->user();
+        return response()->json($notesService->getAllNotes($user));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param Request $request
-     * @param NoteServices $note
+     * @param StoreNoteRequest $request
+     * @param NotesService $notesService
      * @return JsonResponse
      */
-    public function store(Request $request, NoteServices $note): JsonResponse
+    public function store(StoreNoteRequest $request, NotesService $notesService): JsonResponse
     {
-        return response()->json($note->createNote($request));
+        $user = $request->user();
+        $validator = $request->getValidator();
+
+        if ($validator->fails())
+        {
+            return response()->json(['messages' => $validator->errors()],422);
+        }
+
+        $newNote = $notesService->createNote($validator->validated(), $user);
+
+        return response()->json($newNote, 201);
     }
 
     /**
      * Display the specified resource.
      * @param Request $request
-     * @param NoteServices $note
+     * @param NotesService $notesService
      * @param $id
      * @return JsonResponse
      */
-    public function show(Request $request, NoteServices $note,  $id): JsonResponse
+    public function show(Request $request, NotesService $notesService, $id): JsonResponse
     {
-        return response()->json($note->getNote($request, $id));
+        $user = $request->user();
+        $note = $notesService->getNote($id, $user);
+
+        if (!$note)
+        {
+            return response()->json(['message' => self::MESSAGES['notFound']],404);
+        }
+
+        return response()->json($note, 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param Request $request
-     * @param NoteServices $note
+     * @param UpdateNoteRequest $request
+     * @param NotesService $notesService
      * @param $id
      * @return JsonResponse
      */
-    public function update(Request $request, NoteServices $note, $id): JsonResponse
+    public function update(UpdateNoteRequest $request, NotesService $notesService, $id): JsonResponse
     {
-        return response()->json($note->updateNote($request, $id));
+        $user = $request->user();
+        $note = $notesService->getNote($id, $user);
+
+        if (!$note)
+        {
+            return response()->json(['message' => self::MESSAGES['notFound']],404);
+        }
+
+        $validator = $request->getValidator();
+
+        if($validator->fails())
+        {
+            return response()->json(['messages' => $validator->errors()], 422);
+        }
+
+        $updated_note = $notesService->updateNote($note, $validator->validated());
+
+        return response()->json($updated_note);
     }
 
     /**
      * @param Request $request
-     * @param NoteServices $note
+     * @param NotesService $notesService
      * @param $id
      * @return JsonResponse
      */
-    public function destroy(Request $request, NoteServices $note, $id): JsonResponse
+    public function destroy(Request $request, NotesService $notesService, $id): JsonResponse
     {
-        return response()->json($note->deleteNote($request, $id));
+        $user = $request->user();
+        $note = $notesService->getNote($id, $user);
+
+        if (!$note)
+        {
+            return response()->json(['message' => self::MESSAGES['notFound']], 404);
+        }
+
+        $note->delete();
+
+        return response()->json(['message' => self::MESSAGES['deleted']]);
     }
 }
---
+++ app/Services/NotesService.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Models\Note;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use phpDocumentor\Reflection\Types\Mixed_;
+
+class NotesService
+{
+    public function getAllNotes($user): \Illuminate\Database\Eloquent\Collection
+    {
+        return Note::query()->whereIn('category_id', $user->categories->pluck('id'))->get();
+    }
+
+    public function createNote($data, $user): Note
+    {
+        $note = new Note($data);
+        $note->category_id = $data['category_id'];
+        $note->user()->associate($user);
+        $note->save();
+
+        return $note;
+    }
+
+    public function getNote($id, $user)
+    {
+        $note = Note::query()->find($id);
+
+        if (!$note || !$user->categories()->where('id', $note->category_id)->exists())
+        {
+            return null;
+        }
+
+        return $note;
+    }
+
+    public function updateNote($note, $data): Note
+    {
+        $note->category_id = $data['category_id'];
+        $note->update($data);
+
+        return $note;
+    }
+
+    public function deleteNote($request, $id)
+    {
+        $user = $request->user();
+        $note = Note::query()->find($id);
+
+        if (!$note || !$user->categories()->where('id',$note->category_id)->exists())
+        {
+            return ['message' => 'Note not found'];
+        }
+
+        $note->delete();
+
+        return ['message' => 'successfully, note deleted.'];
+    }
+}
